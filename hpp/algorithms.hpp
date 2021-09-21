5f060b4870eb7dab929fcb6e2338dada68505564
Bojan Faletic <bojanf.1996@gmail.com>
2021-09-21 02:47:21 +0200

    algorithms template must be sortable container

--- hpp/algorithms.hpp
+++ hpp/algorithms.hpp
@@ -1,6 +1,14 @@
 #pragma once
 #include <iterator>
 
+// definition of sortable set (with: begging, size and end)
+template <typename T>
+concept is_container = requires(T i) {
+  i.begin();
+  i.end();
+  i.size();
+};
+
 // find minimum element (std::min_element)
 template <typename Iter> Iter min_element(Iter start, Iter end) {
   using typ = typename std::iterator_traits<Iter>::value_type;
@@ -25,7 +33,9 @@ template <typename Iter> void swap(Iter &first, Iter &second) {
 }
 
 // selection sort algorithm: O(n^2)
-template <typename T> void selection(T &list) {
+template <typename T>
+requires is_container<T>
+void selection(T &list) {
   using typ = typename T::iterator;
   typ start_iter = list.begin();
 
@@ -37,7 +47,9 @@ template <typename T> void selection(T &list) {
 }
 
 // bubble sort algorithm: O(n^2)
-template <typename T> void bubble(T &list) {
+template <typename T>
+requires is_container<T>
+void bubble(T &list) {
   using typ = typename T::iterator;
 
   for (unsigned int i = 0; i < list.size() - 1; i++) {
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,36 +1,32 @@
-#include <gtest/gtest.h>
 #include "algorithms.hpp"
 #include <array>
+#include <gtest/gtest.h>
 
-TEST(selection, sort)
-{
-    std::array<int, 3> vv{3, 1, 2};
-    selection(vv);
+TEST(selection, sort) {
+  std::array<int, 3> vv{3, 1, 2};
+  selection(vv);
 
-    std::array<int, 3> correct_vv{1, 2, 3};
-    EXPECT_EQ(vv, correct_vv);
+  std::array<int, 3> correct_vv{1, 2, 3};
+  EXPECT_EQ(vv, correct_vv);
 }
 
-TEST(selection, swap){
-    std::array<int, 2> v{1,2};
-    swap(v[0], v[1]);
+TEST(selection, swap) {
+  std::array<int, 2> v{1, 2};
+  swap(v[0], v[1]);
 
-    std::array<int, 2> v_correct{2, 1};
-    EXPECT_EQ(v, v_correct);
+  std::array<int, 2> v_correct{2, 1};
+  EXPECT_EQ(v, v_correct);
 }
 
+TEST(bubble, sort) {
+  std::array<int, 3> vv{3, 1, 2};
+  bubble(vv);
 
-TEST(bubble, sort)
-{
-    std::array<int, 3> vv{3, 1, 2};
-    bubble(vv);
-
-    std::array<int, 3> correct_vv{1, 2, 3};
-    EXPECT_EQ(vv, correct_vv);
+  std::array<int, 3> correct_vv{1, 2, 3};
+  EXPECT_EQ(vv, correct_vv);
 }
 
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
